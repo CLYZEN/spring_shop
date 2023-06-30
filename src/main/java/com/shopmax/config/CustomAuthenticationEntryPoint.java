4bfe05a76885646da22a7fc13d8441a8b2d0728d
klina8575 <klina8575@gmail.com>
2023-06-30 16:10:12 +0900

    up

--- src/main/java/com/shopmax/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/shopmax/config/CustomAuthenticationEntryPoint.java
@@ -2,7 +2,6 @@ package com.shopmax.config;
 
 import java.io.IOException;
 
-
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -10,13 +9,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+//인증되지 않은 사용자가 리소를 요청할 경우 어떻게 처리할지에 대한 클래스
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"); //401에러
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
 		
 	}
-
+	
 }
