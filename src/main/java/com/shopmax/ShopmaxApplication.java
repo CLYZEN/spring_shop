1b2af8aeb2bdd0dd6aeaab09929bdbafaea14695
clyzen <jimuny01@gmail.com>
2023-10-09 15:57:55 +0900

    commit

--- src/main/java/com/shopmax/ShopmaxApplication.java
+++ src/main/java/com/shopmax/ShopmaxApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopmaxApplication {
 
+	//main 주석 달기
 	public static void main(String[] args) {
 		SpringApplication.run(ShopmaxApplication.class, args);
 	}
--- src/main/java/com/shopmax/entity/Item.java
+++ src/main/java/com/shopmax/entity/Item.java
@@ -3,6 +3,8 @@ package com.shopmax.entity;
 import java.time.LocalDateTime;
 
 import com.shopmax.constant.ItemSellStatus;
+import com.shopmax.dto.ItemFormDto;
+import com.shopmax.exception.OutOfStockException;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -12,7 +14,7 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity {
 	
 	@Id
 	@Column(name="item_id") //테이블로 생성될때 컬럼이름을 지정해준다
@@ -29,16 +31,46 @@ public class Item {
 	private int stockNumber; //재고수량 -> stock_number
 	
 	@Lob //clob과 같은 큰타입의 문자타입으로 컬럼을 만든다
-	@Column(nullable = false)
+	@Column(nullable = false, columnDefinition = "longtext")
 	private String itemDetail; //상품상세설명 -> item_detail
 	
 	@Enumerated(EnumType.STRING) //enum의 이름을 DB의 저장
 	private ItemSellStatus itemSellStatus; //판매상태(SELL, SOLD_OUT) -> item_sell_status
 	
-	private LocalDateTime regTime; //등록시간
+	//item 엔티티 수정
+	public void updateItem(ItemFormDto itemFormDto) {
+		this.itemNm = itemFormDto.getItemNm();
+		this.price = itemFormDto.getPrice();
+		this.stockNumber = itemFormDto.getStockNumber();
+		this.itemDetail = itemFormDto.getItemDetail();
+		this.itemSellStatus = itemFormDto.getItemSellStatus();
+	}
 	
-	private LocalDateTime updateTime; //수정시간
+	//재고를 감소시킨다.
+	public void removeStock(int stockNumber) {
+		int restStock = this.stockNumber - stockNumber; //남은 재고 수량
+		
+		if(restStock < 0) {
+			throw new OutOfStockException("상품의 재고가 부족합니다. "
+					+ "현재 재고수량: " + this.stockNumber);
+		}
+		
+		this.stockNumber = restStock; //남은 재고수량 반영
+	}
+	
+	//재고 증가
+	public void addStock(int stockNumber) {
+		this.stockNumber += stockNumber;
+	}
+	 
 }
+
+
+
+
+
+
+
 
 
 
